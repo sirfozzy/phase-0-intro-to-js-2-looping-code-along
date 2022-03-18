1cb076b67add35281a7ce8a9db1429d1604d04ef
Jason Antman <antmanjt@gmail.com>
2022-03-18 17:07:29 -0400

    commit tree 83af68f479cf5c3cf200465948d10f1d5cab1faa

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const name = ['Lisa', 'Kaitlin', 'Jan'];
 const event = 'surprise';
-const newArray= [];
 
-function writeCards (cards, verb) {
-    for (let i = 0; i <name. length; i++){
-        newArray.push `Thank you, ${name [i]}, for the wonderful ${event} gift=!`
-
-    return newArray
+function writeCards(name, event) {
+    let newArray = []; 
+    for (let i = 0; i < name.length; i++) {
+        newArray.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+        debugger;
     }
 
-writeCards(['Lisa', 'Kaitlin', 'Jan'], 'surprise');
+    return newArray;
 
-function countDown() 
-    let countDown = 10;
-    while (countDown >= 0)
-    console.log(countDown--);
+}
+
+writeCards('name', 'event');
+
+function countDown(init = 10) {
+    while (init >= 0) {
+        console.log(init--);
+    }
 }
